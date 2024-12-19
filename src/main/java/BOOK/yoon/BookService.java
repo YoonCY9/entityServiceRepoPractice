cd35acb02f1a5ceae9841bf950c6ee97343cf005
TJOEUNIS <fana9088@naver.com>
2024-12-19 18:57:01 +0900

    12월 19일 crud 연습

--- src/main/java/BOOK/yoon/BookService.java
+++ src/main/java/BOOK/yoon/BookService.java
@@ -1,7 +1,9 @@
 package BOOK.yoon;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookService {
 
     private final BookRepository repository;
